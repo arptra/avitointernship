ac96b331f4911ad5d0c8c08e65be51490e465105
ai-asp <dukslis777@gmail.com>
2020-05-15 14:38:52 +0300

    refaq

--- main.go
+++ main.go
@@ -1,126 +1,76 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/gorilla/mux"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
 	"time"
 
-	"avitointernship/pkg/ratelimit"
+	"github.com/arptra/avitointernship/pkg/ratelimit"
 )
 
-
-
-type settings	struct {
-	Prefix     int
-	NumCon     int
-	LimitTime  time.Duration
-	BanTime    time.Duration
-	DeleteTime time.Duration
+type infoBan struct {
+	Id       int
+	Subnet   string
+	TimeLeft string
 }
 
-type infoBan	struct {
-	Id			int
-	Subnet		string
-	TimeLeft	string
+type infoIps struct {
+	Id int
+	Ip string
 }
 
-type infoIps	struct {
-	Id			int
-	Ip      	string
+type info struct {
+	Ips     []infoIps
+	Ban     []infoBan
+	ServSet *ratelimit.Settings
 }
 
-type info		struct {
-	Ips			[]infoIps
-	Ban			[]infoBan
-	ServSet		*settings
-}
+var conections = []infoIps{}
 
-func writeIpToFile(ip string, db_name string){
-	file, err := os.OpenFile(db_name, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		file, err := os.Create(db_name)
-		if err != nil{
-			fmt.Println("Unable to create file:", err)
-			os.Exit(1)
-		}
-		file.WriteString(ip)
-		file.WriteString("\n")
-		defer file.Close()
+func addIp(ip string) {
+	var id int
+	if len(conections) == 0 {
+		id = 0
+	} else {
+		id = conections[len(conections)-1].Id
 	}
-	file.WriteString(ip)
-	file.WriteString("\n")
+	id++
+	conections = append(conections, infoIps{id, ip})
 }
 
-func readDB(path string) info {
-	vectorIps := []infoIps{}
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	i := 1
-	for scanner.Scan() {
-		ip := infoIps{}
-		ip.Ip = scanner.Text()
-		ip.Id = i
-		vectorIps = append(vectorIps, ip)
-		i++
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	mu.Lock()
+func getInfo() info {
+	ratelimit.Mu.Lock()
 	vectorBan := []infoBan{}
-	i = 1
-	for subnet, v := range visitors {
-		if v.restrict == true {
+	i := 1
+	for subnet, v := range ratelimit.Visitors {
+		if v.Restrict == true {
 			ban := infoBan{}
 			ban.Subnet = subnet
 			ban.Id = i
-			ban.TimeLeft = time.Now().Sub(v.banTime).String()
+			ban.TimeLeft = time.Now().Sub(v.BanTime).String()
 			vectorBan = append(vectorBan, ban)
 			i++
 		}
 	}
-	mu.Unlock()
-	IpsBan := info{}
-	IpsBan.Ips = vectorIps
-	IpsBan.Ban = vectorBan
-	IpsBan.ServSet = initSettings()
+	ratelimit.Mu.Unlock()
+	IpsBan := info{conections, vectorBan, ratelimit.InitSettings()}
 	return IpsBan
 }
 
 func showIps(w http.ResponseWriter) {
-
-	ips := readDB("db_ip.txt")
-	tmpl, _ := template.ParseFiles("index.html")
+	ips := getInfo()
+	tmpl, _ := template.ParseFiles("./static/index.html")
 	tmpl.Execute(w, ips)
 }
 
 func infoHandler(w http.ResponseWriter, r *http.Request) {
-
-	ip, _ := getIp(r)
+	ip, _ := ratelimit.GetIp(r)
 	if len(ip) > 0 {
-	writeIpToFile(ip, "db_ip.txt")
+		addIp(ip)
 	}
 	showIps(w)
-}
-
-func initSettings() *settings{
-	defVal := &settings{
-					24,
-					100,
-					1*time.Minute,
-					2*time.Minute,
-					1*time.Minute,
-	}
-	return defVal
 }
 
 func DeleteHandler(w http.ResponseWriter, r *http.Request) {
@@ -130,7 +80,7 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	switch r.Method {
 	case "GET":
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, "./static/index.html")
 	case "POST":
 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
@@ -139,9 +89,9 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, "%v\n", r.PostForm)
 		address := r.FormValue("address")
-		mu.Lock()
-		delete(visitors, address)
-		mu.Unlock()
+		ratelimit.Mu.Lock()
+		delete(ratelimit.Visitors, address)
+		ratelimit.Mu.Unlock()
 		fmt.Fprintf(w, "Address = %s have deleted \n", address)
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
@@ -149,11 +99,11 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	params := initSettings()
+	params := ratelimit.InitSettings()
 	fmt.Println("Server is listening...")
 	router := mux.NewRouter()
 	router.HandleFunc("/", infoHandler)
 	router.HandleFunc("/delete", DeleteHandler)
-	http.Handle("/",router)
-	http.ListenAndServe("localhost:8181", limit(router, params))
+	http.Handle("/", router)
+	http.ListenAndServe("localhost:8181", ratelimit.Limit(router, params))
 }
--- pkg/ratelimit/ratelimit.go
+++ pkg/ratelimit/ratelimit.go
@@ -13,28 +13,47 @@ import (
 
 // Create a custom visitor struct which holds the rate limiter for each
 // visitor and the last time that the visitor was seen.
-type visitor	struct {
-	firstSeen	time.Time
-	banTime		time.Time
-	numOfCon	int
-	restrict	bool
+type visitor struct {
+	firstSeen time.Time
+	BanTime   time.Time
+	numOfCon  int
+	Restrict  bool
 }
 
 type errorInfo struct {
-	NumReq		int
-	TimeBan		string
+	NumReq  int
+	TimeBan string
+}
+
+type Settings struct {
+	Prefix     int
+	NumCon     int
+	LimitTime  time.Duration
+	BanTime    time.Duration
+	DeleteTime time.Duration
 }
 
 // Change the the map to hold values of the type visitor.
-var visitors = make(map[string]*visitor)
-var mu sync.Mutex
+var Visitors = make(map[string]*visitor)
+var Mu sync.Mutex
 
 // Run a background goroutine to remove old entries from the visitors map.
 func init() {
 	go cleanupVisitors()
 }
 
-func getIp(req *http.Request) (string, error)  {
+func InitSettings() *Settings {
+	defVal := &Settings{
+		24,
+		100,
+		1 * time.Minute,
+		2 * time.Minute,
+		1 * time.Minute,
+	}
+	return defVal
+}
+
+func GetIp(req *http.Request) (string, error) {
 	ips := req.Header.Get("X-FORWARDED-FOR")
 	splitIps := strings.Split(ips, ",")
 	for _, ip := range splitIps {
@@ -53,28 +72,28 @@ func IpSubNet(ip string, prefix int) string {
 	return IpSub
 }
 
-func getVisitor(ip string, params* settings) *visitor {
-	mu.Lock()
-	defer mu.Unlock()
+func getVisitor(ip string, params *Settings) *visitor {
+	Mu.Lock()
+	defer Mu.Unlock()
 
 	rateLim := params.LimitTime
 	banTime := params.BanTime
-	prefix	:= params.Prefix
-	numCon	:= params.NumCon
+	prefix := params.Prefix
+	numCon := params.NumCon
 	IpSub := IpSubNet(ip, prefix)
-	v, exists := visitors[IpSub]
+	v, exists := Visitors[IpSub]
 	if !exists {
 		// Include the current time when creating a new visitor.
-		visitors[IpSub] = &visitor{time.Now(), time.Now(), 1, false}
-		return visitors[IpSub]
+		Visitors[IpSub] = &visitor{time.Now(), time.Now(), 1, false}
+		return Visitors[IpSub]
 	}
 	v.numOfCon++
-	if v.restrict == false && (v.numOfCon >= numCon && time.Since(v.firstSeen) <= rateLim) {
+	if v.Restrict == false && (v.numOfCon >= numCon && time.Since(v.firstSeen) <= rateLim) {
 		fmt.Println(v.numOfCon)
-		v.restrict = true
-		v.banTime = time.Now()
-	} else if v.restrict == true && time.Since(v.banTime) > banTime {
-		delete(visitors, IpSub)
+		v.Restrict = true
+		v.BanTime = time.Now()
+	} else if v.Restrict == true && time.Since(v.BanTime) > banTime {
+		delete(Visitors, IpSub)
 	}
 	return v
 }
@@ -82,24 +101,23 @@ func getVisitor(ip string, params* settings) *visitor {
 // Every minute check the map for visitors that haven't been seen for
 // more than 2 minutes and delete the entries.
 func cleanupVisitors() {
-	params := initSettings()
+	params := InitSettings()
 	for {
 		time.Sleep(params.DeleteTime)
-
-		mu.Lock()
+		Mu.Lock()
 		banTime := params.BanTime
-		for ip, v := range visitors {
-			if time.Since(v.banTime) > banTime {
-				delete(visitors, ip)
+		for ip, v := range Visitors {
+			if time.Since(v.BanTime) > banTime {
+				delete(Visitors, ip)
 			}
 		}
-		mu.Unlock()
+		Mu.Unlock()
 	}
 }
 
-func limit(next http.Handler, params* settings) http.Handler {
+func Limit(next http.Handler, params *Settings) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip, err := getIp(r)
+		ip, err := GetIp(r)
 		if err != nil {
 			ip, _, err = net.SplitHostPort(r.RemoteAddr)
 			if err != nil {
@@ -109,8 +127,8 @@ func limit(next http.Handler, params* settings) http.Handler {
 			}
 		}
 		limiter := getVisitor(ip, params)
-		if limiter.restrict == true {
-			tmpl, _ := template.ParseFiles("429.html")
+		if limiter.Restrict == true {
+			tmpl, _ := template.ParseFiles("./static/429.html")
 			w.WriteHeader(429)
 			desc := errorInfo{}
 			desc.NumReq = params.NumCon
